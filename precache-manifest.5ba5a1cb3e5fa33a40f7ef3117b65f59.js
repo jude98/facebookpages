3774b8f848035d2098da8ac1e86fd208d56a70d7
jude98 <judithgeorgejoseph123@gmail.com>
2020-09-16 16:03:44 +0530

    Updates

---
+++ precache-manifest.5ba5a1cb3e5fa33a40f7ef3117b65f59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09b1a348381ad99eaa2c733f389fd39c",
+    "url": "/facebookpages/index.html"
+  },
+  {
+    "revision": "3e26d4da6b094434a443",
+    "url": "/facebookpages/static/css/main.5edaff52.chunk.css"
+  },
+  {
+    "revision": "6ef3234de64375fc4eba",
+    "url": "/facebookpages/static/js/2.faf16075.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/facebookpages/static/js/2.faf16075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e26d4da6b094434a443",
+    "url": "/facebookpages/static/js/main.15cf3ed8.chunk.js"
+  },
+  {
+    "revision": "613b0e26438297014a4b",
+    "url": "/facebookpages/static/js/runtime-main.bd92c902.js"
+  }
+]);
